26b384edcfa7437db3a3d89cba0441ff196e8e96
Raeed Chowdhury <raeed.chowdhury@gmail.com>
2019-02-12 15:15:42 -0600

    refactor(tuning plots): make tuning plots more flexible to inputs
    BREAKING CHANGE: this commit changes how arguments are passed into plotTuning and compareTuning

--- compareTuning.m
+++ compareTuning.m
@@ -1,80 +1,94 @@
-function compareTuning(curves,pds,params)
+function compareTuning(neuron_table,params)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %   comares tuning between different conditions with empirical
 %   tuning curves and PDs.
 %   Inputs - 
-%       curves - cell array of tuning curve tables, one table
-%                   per condition
-%       pds - cell array of PD tables, one table per condition
+%       neuron_table - table of neurons with tuning curves and pds
 %       params - parameters struct
-%           .which_units - (optional) vector array unit indices to plot
-%                       default - plots them all
+%           .cond_cols - column names of table that define the condition
+%           .signalIDs - (optional) matrix where rows indicate which signalIDs
+%                       to plot. default - plots them all
 %           .maxFR - (optional) array of maximum firing rates to
-%                   display in polar plots. Default behavior is to
-%                   find maximum firing rate over all curve
-%                   conditions
-%           .move_corr - move correlate to compare tuning over
-%               (default - 'vel')
+%                   display in polar plots. Default behavior is currently broken
+%           .curve_colname - name of tuning curve column (default - 'velCurve')
+%           .pd_colname - name of PD column (default - 'velPD')
 %           .cond_colors - colors for conditions
 %               (default - linspecer(num_conditions)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % initial check
-assert(iscell(curves) && iscell(pds),'Curves and PDs must be passed in as cell arrays')
+assert(istable(neuron_table),'neuron_table must be a table!')
 
 % Default Params
-which_units = 1:height(curves{1});
-move_corr = 'vel';
-cond_colors = linspecer(numel(curves));
+cond_cols = {};
+signalIDs = unique(neuron_table.signalID,'rows');
+curve_colname = 'velCurve';
+pd_colname = 'velPD';
+cond_colors = [];
 % get maxFR for each neuron
-maxFR = max(cell2mat(cellfun(@(x) x.([move_corr 'CurveCIhigh']),reshape(curves,1,[]),'UniformOutput',false)),[],2);
+maxFR = 0;
 
-if nargin > 2, assignParams(who,params); end % overwrite parameters
+assignParams(who,params)
+
+% get information about table
+conditions = unique(neuron_table(:,cond_cols),'rows');
+if isempty(cond_colors)
+    cond_colors = linspecer(height(conditions));
+end
 
 % check inputs
 if numel(maxFR) == 1
-    maxFR = repmat(maxFR,height(curves{1}),1);
+    maxFR = repmat(maxFR,size(signalIDs,1),1);
 end
-assert(isempty(maxFR) || numel(maxFR)==height(curves{1}),'maxFR is wrong size')
-assert(isvector(which_units),'which_units needs to be vector')
-assert(size(cond_colors,1)==numel(curves),'Number of colors must match number of conditions')
-
-% make curves a row cell array
-curves = reshape(curves,1,length(curves)); % this should throw an error if it's not a 1-d cell array
+assert(numel(maxFR)==size(signalIDs,1),'maxFR is wrong size')
+assert(size(cond_colors,1)==height(conditions),'Number of colors must match number of conditions')
 
 %% Plot tuning curves
 % number of subplots (include plot for legends)
-n_rows = ceil(sqrt(length(which_units)+1));
-% get signal ID
-signalID = curves{1}.signalID;
+n_rows = ceil(sqrt(size(signalIDs,1)+1));
 % make plots
-for neuron_idx = 1:length(which_units)
+for neuron_idx = 1:size(signalIDs,1)
     subplot(n_rows,n_rows,neuron_idx)
-    for cond_idx = 1:numel(curves)
-        pdTable = pds{cond_idx};
-        curveTable = curves{cond_idx};
-        plotFlatTuning(pdTable(which_units(neuron_idx),:),curveTable(which_units(neuron_idx),:),...
-            maxFR(which_units(neuron_idx)),cond_colors(cond_idx,:),[], move_corr);
-        % plotTuning(pdTable(which_units(neuron_idx),:),curveTable(which_units(neuron_idx),:),...
-        %     maxFR(which_units(neuron_idx)),cond_colors(cond_idx,:),[], move_corr);
-        % plot only PDs
-        % plotTuning(pdTable(which_units(neuron_idx),:),[],...
-        %     maxFR(which_units(neuron_idx)),cond_colors(cond_idx,:),[], move_corr);
+
+    % get maxFR for each neuron if not passed in
+    if maxFR(neuron_idx) == 0
+        [~,temp_table] = getNTidx(neuron_table,'signalID',signalIDs(neuron_idx,:));
+        if ismember(sprintf('%sCIhigh',curve_colname),temp_table.Properties.VariableNames)
+            maxFR(neuron_idx) = max(max(temp_table.(sprintf('%sCIhigh',curve_colname))));
+        else
+            maxFR(neuron_idx) = max(max(temp_table.(curve_colname)));
+        end
+    end
+
+    for cond_idx = 1:height(conditions)
+        % put cond entry into cell for expansion...
+        cond_cell = [conditions.Properties.VariableNames; table2cell(conditions(cond_idx,:))];
+        % get all entries pertaining to condition and neuron
+        [~,temp_table] = getNTidx(neuron_table,'signalID',signalIDs(neuron_idx,:),cond_cell{:});
+
+        plotTuning(temp_table,...
+            struct('maxFR',maxFR(neuron_idx),...
+                'unroll',true,...
+                'color',cond_colors(cond_idx,:),...
+                'plot_ci',false,...
+                'pd_colname',pd_colname,...
+                'curve_colname',curve_colname));
         hold on
     end
-    if isnumeric(signalID(which_units(neuron_idx)))
-        label = ['Neuron ' num2str(signalID(which_units(neuron_idx)))];
+    if isnumeric(signalIDs(neuron_idx,:))
+        label = ['Neuron ' num2str(signalIDs(neuron_idx,:))];
     else
-        label = ['Neuron ' signalID(which_units(neuron_idx))];
+        label = ['Neuron ' signalIDs(neuron_idx,:)];
     end
 
     title(label)
 end
 
+% for a legend...
 subplot(n_rows,n_rows,n_rows^2)
-for cond_idx = 1:numel(curves)
+for cond_idx = 1:height(conditions)
     plot([0 1],repmat(cond_idx,1,2),'-','linewidth',2,'color',cond_colors(cond_idx,:))
     hold on
 end
 ylabel 'Condition number'
-set(gca,'box','off','tickdir','out','xtick',[],'ytick',1:numel(curves))
+set(gca,'box','off','tickdir','out','xtick',[],'ytick',1:height(conditions))
--- plotTuning.m
+++ plotTuning.m
@@ -1,46 +1,168 @@
-function plotTuning(pdData,curve,maxRadius,color,linspec, move_corrIn)
-% PLOT_TUNING makes a single figure showing the tuning curve and PD with
-% confidence intervals. Leave either entry blank to skip plotting it. Color
+function plotTuning(neuron_table,params)
+% PLOTFLATTUNING makes a single figure showing the tuning curve and PD with
+% confidence intervals, unwrapped. Leave either entry blank to skip plotting it. Color
 % is a 3 element vector for the color of the plotted tuning curve and PD.
 % pdData is one row taken from binnedData object.
-move_cor = 'vel';
-if nargin >5, move_cor = move_corrIn; end 
-% plot initial point
-h=polar(0,maxRadius);
-set(h,'color','w')
-hold all
+% Inputs:
+%   pdData - table including PDs for one unit. Should either be one row or
+%       several rows with the same signalID
+%   curve - table including tuning curve for one unit. Should either be one row or
+%       several rows with the same signalID
+%   params - parameters struct
+%       .maxFR - (numeric) maximum value to plot (no default)
+%       .unroll - (bool) whether to plot a flat curve or polar curve (default: false)
+%       .color - (numeric or char) specify color to plot
+%       .pd_linspec - (char) line specification for plot function for PD (default: '-')
+%       .pd_colname - (char) name of PD column (default: 'velPD')
+%       .curve_colname - (char) name of curve column (default: 'velCurve')
+%       .plot_ci - (bool) whether to plot confidence interval (default: true)
 
-if ~exist('linspec','var') || isempty(linspec)
-    linspec = '-';
+maxFR = [];
+unroll = false;
+curve_colname = 'velCurve';
+assignParams(who,params)
+
+if isempty(maxFR)
+    if ismember(sprintf('%sCIhigh',curve_colname),neuron_table.Properties.VariableNames)
+        maxFR = max(max(neuron_table.(sprintf('%sCIhigh',curve_colname))));
+    else
+        maxFR = max(max(neuron_table.(curve_colname)));
+    end
+    % add to params for later
+    params.maxFR = maxFR;
+end
+assert(maxFR>0, 'Need to provide a non-zero maxFR!')
+
+hold on
+if ~unroll
+    % plot initial point
+    h=polar(0,maxFR);
+    set(h,'color','w')
 end
 
-% tuning curve
-if(~isempty(curve))
-    if(height(curve)>1)
-        error('plotTuning:TooManyThings','curve must contain only one row')
-    end
-    bins = curve.bins;
-    h=polar(repmat(bins,1,2),repmat(curve.binnedResponse,1,2));
-    set(h,'linewidth',2,'color',color)
-    th_fill = [fliplr(bins) bins(end) bins(end) bins];
-    r_fill = [fliplr(curve.CIhigh) curve.CIhigh(end) curve.CIlow(end) curve.CIlow];
-    [x_fill,y_fill] = pol2cart(th_fill,r_fill);
-    patch(x_fill,y_fill,color,'facealpha',0.3,'edgealpha',0);
-    % plot so things work in illustrator
-    % plot(x_fill,y_fill,'color',color,'linewidth',1.85);
+% make sure there's only one unit here
+sigIDs = unique(neuron_table.signalID,'rows');
+if size(sigIDs,1)>1
+    error('plotTuning:TooManyThings','neuron_table must contain only one unique signalID')
 end
 
-% PD
-if(~isempty(pdData))
-    if(height(pdData)>1)
-        error('plotTuning:TooManyThings','pdData must contain only one row')
+% do the plot
+if height(neuron_table)==1
+    plot_curve(neuron_table,params);
+    plot_pd(neuron_table,params);
+else
+    % plot individual traces without CI (for now)
+    indiv_params = params;
+    indiv_params.plot_ci = false;
+    for signum = 1:height(neuron_table)
+        p = plot_curve(neuron_table(signum,:),indiv_params);
+        set(p,'linewidth',0.45)
+        p = plot_pd(neuron_table(signum,:),indiv_params);
+        set(p,'linewidth',0.44)
     end
-    h=polar(repmat(pdData.([move_cor 'PD']),2,1),maxRadius*[0;1],linspec);
-    set(h,'linewidth',2,'color',color)
-    th_fill = [0 pdData.([move_cor, 'PDCI'])(2) pdData.([move_cor 'PD']) pdData.([move_cor, 'PDCI'])(1) 0];
-    r_fill = [0 maxRadius maxRadius maxRadius 0];
-    [x_fill,y_fill] = pol2cart(th_fill,r_fill);
-    patch(x_fill,y_fill,color,'edgecolor','none','facealpha',0.3);
-    % plot so things work in illustrator
-    % plot(x_fill,y_fill,'color',color,'linewidth',1.85);
+
+    % plot average curve in thicker line
+    avg_table = neuronAverage(neuron_table,struct('keycols','signalID'));
+    p = plot_curve(avg_table,indiv_params);
+    set(p,'linewidth',2.1,'color','k')
+
+    % plot average pd in thicker line
+    p = plot_pd(avg_table,indiv_params);
+    set(p,'linewidth',2.09,'color','k')
+end
+
+% plot settings
+if unroll
+    set(gca,'box','off','tickdir','out',...
+        'xlim',[-pi pi],'ylim',[0 maxFR],...
+        'xtick',[-pi, 0, pi],'xticklabel',{'-180','','180'})
+end
+
+end
+
+function p = plot_curve(curve,params)
+    % plotting params
+    unroll = false;
+    curve_colname = 'velCurve';
+    color = 'k';
+    plot_ci = true;
+    assignParams(who,params)
+
+    % only run if the column name is there
+    if ismember(curve_colname,curve.Properties.VariableNames)
+        bins = curve.bins;
+        th_wrap = [bins-2*pi bins bins+2*pi];
+
+        if plot_ci
+            th_fill = [th_wrap fliplr(th_wrap)];
+            r_fill = [repmat(curve.(sprintf('%sCIlow',curve_colname)),1,3),...
+                fliplr(repmat(curve.(sprintf('%sCIhigh',curve_colname)),1,3))];
+            th_fill = th_fill(~isnan(r_fill));
+            r_fill = r_fill(~isnan(r_fill));
+            % h=plot(th_fill,r_fill);
+            % set(h,'linewidth',1.2,'color',color)
+            if unroll
+                patch(th_fill,r_fill,color,'edgealpha',0,'facealpha',0.3);
+            else
+                [x_fill,y_fill] = pol2cart(th_fill,r_fill);
+                patch(x_fill,y_fill,color,'facealpha',0.3,'edgealpha',0);
+            end
+        end
+
+        curve_wrap = repmat(curve.(curve_colname),1,3);
+        th_wrap = th_wrap(~isnan(curve_wrap));
+        curve_wrap = curve_wrap(~isnan(curve_wrap));
+        if unroll
+            p = plot(th_wrap,curve_wrap,'linewidth',2,'color',color);
+        else
+            p=polar(th_wrap,curve_wrap);
+            set(p,'linewidth',2,'color',color)
+        end
+    end
+end
+
+function p = plot_pd(pdData,params)
+    % plotting params
+    maxFR = 0;
+    unroll = false;
+    pd_colname = 'velPD';
+    color = 'k';
+    pd_linspec = '-';
+    plot_ci = true;
+    assignParams(who,params)
+
+    % only run if column name is there
+    if ismember(pd_colname,pdData.Properties.VariableNames)
+        if plot_ci
+            % handle wraparound
+            if pdData.(sprintf('%sCI',pd_colname))(2)<pdData.(sprintf('%sCI',pd_colname))(1)
+                pdData.(sprintf('%sCI',pd_colname))(2) = pdData.(sprintf('%sCI',pd_colname))(2) + 2*pi;
+            end
+            th_fill = [pdData.(sprintf('%sCI',pd_colname))(1),...
+                pdData.(sprintf('%sCI',pd_colname))(1),...
+                pdData.(sprintf('%sCI',pd_colname))(2),...
+                pdData.(sprintf('%sCI',pd_colname))(2)];
+            r_fill = [0 maxFR maxFR 0];
+            if unroll
+                % h=plot(th_fill,r_fill);
+                % set(h,'linewidth',1.2,'color',color)
+                patch(th_fill,r_fill,color,'edgealpha',0,'facealpha',0.3);
+                % plot wraparound
+                % h=plot(th_fill-2*pi,r_fill);
+                % set(h,'linewidth',1.2,'color',color)
+                patch(th_fill-2*pi,r_fill,color,'edgealpha',0,'facealpha',0.3);
+            else
+                [x_fill,y_fill] = pol2cart(th_fill,r_fill);
+                patch(x_fill,y_fill,color,'edgecolor','none','facealpha',0.3);
+            end
+        end
+
+        if unroll
+            p=plot(repmat(pdData.(pd_colname),2,1),maxFR*[0;1],pd_linspec);
+            set(p,'linewidth',2,'color',color)
+        else
+            p=polar(repmat(pdData.(pd_colname),2,1),maxFR*[0;1],linspec);
+            set(p,'linewidth',2,'color',color)
+        end
+    end
 end
